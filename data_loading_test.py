33300946994a3b8e6f744915084f0d7810a0573e
reggiemclean <reginaldmclean@trentu.ca>
2020-10-13 13:00:57 -0400

    First commit, data loading

---
+++ data_loading_test.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import collections as c
+
+
+data = np.genfromtxt("../games.csv", delimiter=',', dtype=None, encoding=None, names=True)
+df = pd.DataFrame(data)
+
+ids = df['HOME_TEAM_ID'].unique()
+team_ids_dict = {}
+for id in ids:
+    team_dates = []
+    for index, row in df.iterrows():
+        if row['HOME_TEAM_ID'] == id:
+            team_dates.append(row['GAME_DATE_EST'])
+    team_dates.sort()
+    ordered_dictionary = c.OrderedDict()
+    for date in team_dates:
+        ordered_dictionary[date] = df[(df['HOME_TEAM_ID'] == id) & (df['GAME_DATE_EST'] == date)]
+    team_ids_dict[id] = ordered_dictionary
+
+# need to now use OG data to build data pipeline that can take parameter X and generate data going back X days
